73fa21fd00e02cae7077106586ce743de962ac3a
Alan <alanlossendiere@gmail.com>
2023-07-28 00:23:00 -0300

    Agregado de datos a lugares.js en .JSON

---
+++ src/data/lugares.js
@@ -0,0 +1,36 @@
+export const departamento = {
+  titulo: "Departamento Denalan",
+  descripcion:
+    "Una vivienda especial para jovenes o para familia de hasta 6 miembros, muy comodo por la ubicacion del centro de gesell y por sobre todo, por las playas geselinas, tenemos cocina y todos los utensillos para poder cocinar.",
+  comodidad: "1 cama matrimonial, 1 cama litera, 2 sillon-cama",
+  ubicacion:
+    "Nos encontramos sobre la Avenida 3 entre paseo 104 y 102. Estamos en zona centro 🌃 2 cuadras del mar 🏖️ 50 mts de la peatonal ⚡️",
+  Servicios:
+    'Contamos con Wi-Fi 💯 LED 32" con chromecast Aire Acondicionado 🔥 ❄️',
+  Garage:
+    "¡¡No tenemos estacionamiento por lo cual el auto debe quedar estacionado en la calle o en algun estacionamiento de la zona!!",
+};
+
+export const casaTintaRoja = {
+  titulo: "Casa Tinta Roja",
+  descripcion:
+    "Es una casa caracterizada por la *exclusividad* familiar, con capacidad de hasta 9 personas. La casa se ubica en la zona Centro-Sur de la ciudad de Villa Gesell, zona muy tranquila ideal para descansar con familia o con otra familia mas.",
+  comodidad: "2 Matroniales 2 Literas 2 Simples",
+  ubicacion:
+    "Av. 7 E/ Paseo 127 y 128 Centro-Sur de Villa Gesell 7 cuadras del mar 🏖️ 3 cuadras de zona comercial 🌃 20 cuadras de peatonal ⚡️",
+  Servicios: 'Contamos con Wi-Fi 💯 LED 32" con chromecast Ping-Pong 🏓',
+  Garage: "Entrada para 2 o 3 autos 🚗 🚙",
+};
+
+export const casaTintaRoja2 = {
+  titulo: "Casa Tinta Roja 2",
+  descripcion:
+    "Es una cabaña excelente para descansar, es ideal para una pareja, o una familia pequeña de hasta 3 o 4 personas por la cama individual corrediza que tenemos 😎. Caracterizado por la comodidad y la tranquilidad de la zona.",
+  comodidad: "1 cama matrimonial 1 cama individual 1 cama individual corrediza",
+  ubicacion:
+    "Sobre la avenida 7 entre paseos 127 y 128 Zona centro-sur de la ciudad 🌇 7 cuadras del mar 🏖️20 cuadras de la peatonal",
+  Servicios:
+    'Tenemos Wi-Fi 💯 LED 32" con chromecast Aire Acondicionado 🔥 ❄️ Parrilla 😏',
+  Garage:
+    "Entrada hasta para 2 autos 🚙 🚗 Parque amplio con pasto 100% Natural 🌱 ☀️",
+};
